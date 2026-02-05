fbe1a0d3d887ada7a50be8d09930a8ee910a8bee
ril7rti <ril7rti@gmail.com>
2026-02-05 14:51:39 +0900

    Create draw.js

---
+++ js/render/draw.js
@@ -0,0 +1,17 @@
+import { drawGrid } from "./drawGrid.js";
+import { drawPath } from "./drawPath.js";
+import { drawTowers } from "./drawTowers.js";
+import { drawEnemies } from "./drawEnemies.js";
+import { drawBullets } from "./drawBullets.js";
+import { drawCanvasUI } from "./drawUI.js";
+
+export function draw(ctx, state, W, H) {
+  ctx.clearRect(0, 0, W, H);
+
+  drawGrid(ctx, W, H);
+  drawPath(ctx);
+  drawTowers(ctx, state.towers);
+  drawEnemies(ctx, state.enemies);
+  drawBullets(ctx, state.bullets);
+  drawCanvasUI(ctx, state, W, H);
+}
