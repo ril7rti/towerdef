e19c30201a912becbb4a1318c2ab940cddc3a6fc
ril7rti <ril7rti@gmail.com>
2026-02-05 14:45:15 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+export const CANVAS_W = 900;
+export const CANVAS_H = 520;
+
+export const CELL = 40;
+export const PATH_HALF = 22;
+
+export const DT_MAX = 0.033;
+
+export const START_LIVES = 10;
+export const START_GOLD = 100;
+
+export const TOWER_COST_START = 50;
+export const TOWER_COST_STEP = 5;
+export const TOWER_COST_MAX = 120;
+
+export const BULLET_SPEED = 320;
